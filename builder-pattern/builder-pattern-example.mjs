4984a8659d0ce02459b322125cfb707ca43276e2
Artem5725 <roadfromreversertowebdesigner@mail.ru>
2022-12-18 00:30:47 +0300

    builder pattern (fix)

--- builder-pattern/builder-pattern-example.mjs
+++ builder-pattern/builder-pattern-example.mjs
@@ -1,10 +1,14 @@
 class BlockBase {
-    constructor(backgroundColor, borderColor, optionalFields) {
+    constructor(backgroundColor, borderColor, optionalFields) { // опциональные поля передаются как массив пар
         let elem = document.createElement('div');
         elem.style.backgroundColor = backgroundColor;
         elem.style.borderColor = borderColor;
-        for (let field in optionalFields) {
-            elem.style[field] = optionalFields[field];
+        elem.style.borderStyle = "solid";
+        elem.style.borderWidth = "4px";
+        elem.style.width = "100px"; // дефолтные значения, которые можно менять через ввод
+        elem.style.height = "100px";
+        for (let field of optionalFields) {
+            elem.style[field[0]] = field[1];
         }
         return elem;
     }
@@ -17,23 +21,25 @@ class BlockBuilder {
     }
 
     setWidth(value) {
-        this.width = value;
+        this.width = value + "px";
+        return this;
     }
 
     setHeight(value) {
-        this.height = value;
+        this.height = value + "px";
+        return this;
     }
 
     build() {
-        return new BlockBase(this.reqBackgroundColor, this.reqBorderColor, Object.entries(this).filter(param => {
+        return new BlockBase(this.reqBackgroundColor, this.reqBorderColor, Object.entries(this).filter(param =>
             !param[0].startsWith("req") // обязательный параметры помечены префиксом req
-        }))
+        ));
     }
 }
 
 function startBuilding() {
     let reqBackgroundColor = document.getElementById('create-background-color').value;
-    let reqBorderColor = document.getElementById('create-background-color').value;
+    let reqBorderColor = document.getElementById('create-border-color').value;
     let optWidth = document.getElementById('create-width').value;
     let optHeight = document.getElementById('create-height').value;
     let block = new BlockBuilder(reqBackgroundColor, reqBorderColor);
